f9fda2687e605c0e845a741e6c81625f662dcd49
yaobin1340 <645894453@qq.com>
2016-04-12 09:41:03 +0800

    1

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -34,6 +34,52 @@ class MY_Controller extends CI_Controller
 //		$this->cismarty->show($html);
 //	}
 
+	/**
+	 * 提示信息
+	 * @param varchar $message 提示信息
+	 * @param varchar $url 跳转页面，如果为空则后退
+	 * @param int $type 提示类型，1是自动关闭的提示框，2是错误提示框
+	 * @return array 显示页码的数组
+	 **/
+	public function show_message($message,$url=null,$type = 1){
+		if($url){
+			$js = "location.href='".$url."';";
+		}else{
+			$js = "history.back();";
+		}
+
+		if($type=='1'){
+			echo "<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'>
+				<html xmlns='http://www.w3.org/1999/xhtml'>
+				<head>
+				<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+				<meta name='viewport' content='width=device-width, initial-scale=1.0, minimum-scale=0.5, maximum-scale=2.0, user-scalable=yes' />
+				<title>".$message."</title>
+				<script src='../../static/js/jquery.min.js'></script>
+				<link rel='stylesheet' href='../../res/css/dialog.css'>
+				</head>
+				<body>
+				<script src='../../res/js/easydialog.min.js'></script>
+				<script>
+				var callFn = function(){
+				  ".$js."
+				};
+				easyDialog.open({
+					container : {
+						content : '".$message."'
+					},
+					autoClose : 2000,
+					callback : callFn
+
+				});
+
+				</script>
+				</body>
+				</html>";
+		}
+		exit;
+	}
+
 
 }
 
